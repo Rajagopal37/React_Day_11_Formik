8fdaa69fc3fc9480ee2c0acb52a7ae98475b8e74
Rajagopal37 <136100642+Rajagopal37@users.noreply.github.com>
2024-09-06 11:02:35 +0530

    Update Dashboard.jsx

--- formik/src/components/Dashboard.jsx
+++ formik/src/components/Dashboard.jsx
@@ -3,6 +3,13 @@ import BookManagement from "./BookMAnagement";
 import AuthorManagement from "./AuthorManagement";
 
 const Dashboard = () => (
+  // <div className="container mt-4">
+  //   <h1>Library Management Dashboard</h1>
+  //   <div className="d-flex justify-content-center align-items-center">
+  //     <BookManagement />
+  //     <AuthorManagement />
+  //   </div>
+  // </div>
   <div className="container mt-4">
     <h1>Library Management Dashboard</h1>
     <div className="d-flex justify-content-center align-items-center">
